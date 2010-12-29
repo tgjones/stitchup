f3ebdc696c63aa3a9c18db396832b79c55a87c68
Tim Jones <github@roastedamoeba.com>
2010-12-29 22:03:23 +0800

    Removed surface shaders because the implementation is too immature to be useful (at this stage)

--- src/StitchUp.SurfaceShaders.Content.Pipeline/Shaders/LightingModelBlockNode.cs
+++
@@ -1,9 +0,0 @@
-using StitchUp.Content.Pipeline.FragmentLinking.CodeModel;
-
-namespace StitchUp.SurfaceShaders.Content.Pipeline.Shaders
-{
-	public class LightingModelBlockNode : ParseNode
-	{
-		public string LightingModel { get; set; }
-	}
-}
--- src/StitchUp.SurfaceShaders.Content.Pipeline/Shaders/ShaderContent.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.Xna.Framework.Content.Pipeline;
-
-namespace StitchUp.SurfaceShaders.Content.Pipeline.Shaders
-{
-	public class ShaderContent : ContentItem
-	{
-		public ShaderNode ShaderNode { get; set; }
-	}
-}
